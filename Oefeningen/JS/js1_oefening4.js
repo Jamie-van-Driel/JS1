629332743ada1ca0e4ccdb01fef69597a2916cad
Jamie-van-Driel <92149199+Jamie-van-Driel@users.noreply.github.com>
2025-09-23 11:56:39 +0200

    Oefening 2.1

---
+++ Oefeningen/JS/js1_oefening4.js
@@ -0,0 +1,11 @@
+//js1_oefening4.js
+
+//Variabelen aanmaken
+let myNumber = 20;
+let myString = "Jamie";
+let myBoolean = true;
+
+//Console.log aanmaken
+console.log("Ik ben " + myNumber + " jaar oud.");
+console.log("Mijn naam is " + myString);
+console.log("Heb ik een huisdier? " + myBoolean);
